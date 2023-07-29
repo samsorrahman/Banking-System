70a8cd3b0065a4a4bc3704eb5ce19ff37610ab71
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-07-29 10:27:58 -0700

    user registration complete setup and functionality

--- userauths/views.py
+++ userauths/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from userauths.forms import UserRegisterForm
+from django.contrib.auth import authenticate, login
 
+from django.contrib import messages
 # Create your views here.
+
+
+def RegisterView(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            # form.save()
+            new_user = form.save() # new_user.email
+            username = form.cleaned_data.get("username")
+            # username = request.POST.get("username")
+            messages.success(request, f"Hey {username}, your account was created successfully.")
+            # new_user = authenticate(username=form.cleaned_data.get('email'))
+            new_user = authenticate(username=form.cleaned_data['email'],
+                                    password=form.cleaned_data['password1'])
+            login(request, new_user)
+            return redirect("core:index")
+    
+    if request.user.is_authenticated:
+        messages.warning(request, f"You are already logged in.")
+        return redirect("core:index")
+
+
+    else:
+        form = UserRegisterForm()
+    context = {
+        "form": form
+    }
+    return render(request, "userauths/sign-up.html", context)
