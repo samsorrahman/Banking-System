78747f9a11ad34239987831e8a4077d651080c03
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-07-31 13:02:07 -0700

    kyc view alerts and more

--- payment_prj/urls.py
+++ payment_prj/urls.py
@@ -16,10 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from core import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('userauths.urls')),
     path('', include("core.urls")),
+    path('user/', include('userauths.urls')),
+    path('account/', include("account.urls")),
+    
+    
 ]
+
+
+if settings.DEBUG:
+    urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
