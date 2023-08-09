1ed507adf1c4d24aa62d566a0043e9d81fcf957a
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-08-09 04:22:21 -0700

    delete sent request

--- core/payment_request.py
+++ core/payment_request.py
@@ -179,3 +179,13 @@ def SettlementCompleted(request, account_number ,transaction_id):
             "transaction":transaction,
         }
     return render(request, "payment_request/settlement-completed.html", context)
+
+
+def deletepaymentrequest(request, account_number ,transaction_id):
+    account = Account.objects.get(account_number=account_number)
+    transaction = Transaction.objects.get(transaction_id=transaction_id)
+
+    if request.user == transaction.user:
+        transaction.delete()
+        messages.success(request, "Payment Request Deleted Sucessfully")
+        return redirect("core:transactions")
