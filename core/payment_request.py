89d0f1cbd7b62de81b5bf12c91dfa3e2b5ae8190
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-08-09 03:24:16 -0700

    confirm setlement request made by user

--- core/payment_request.py
+++ core/payment_request.py
@@ -120,3 +120,19 @@ def RequestCompleted(request, account_number ,transaction_id):
             "transaction":transaction,
         }
     return render(request, "payment_request/amount-request-completed.html", context)
+
+
+
+
+
+################################## Settled ##########################3
+def settlement_confirmation(request, account_number ,transaction_id):
+    account = Account.objects.get(account_number=account_number)
+    transaction = Transaction.objects.get(transaction_id=transaction_id)
+    
+    context = {
+            "account":account,
+            "transaction":transaction,
+        }
+    return render(request, "payment_request/settlement-confirmation.html", context)
+
